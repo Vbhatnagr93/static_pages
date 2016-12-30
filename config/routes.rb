974c56f9c05351c1487c411cc6fb08f317b5227d
vivek <vivek.bhatnagar93@gmail.com>
2016-12-30 11:40:53 -0600

    finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get 'staticpages/help'
 
+  get 'staticpages/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'application#hello'
 
